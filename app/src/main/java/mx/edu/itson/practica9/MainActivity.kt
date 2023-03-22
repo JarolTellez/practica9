a608c084c523d591b32d211064a93be1c59230e9
Jarol <jarol.tellez228188@potros.itson.edu.mx>
2023-03-22 11:30:57 -0800

    Initial commit

---
+++ app/src/main/java/mx/edu/itson/practica9/MainActivity.kt
@@ -0,0 +1,38 @@
+package mx.edu.itson.practica9
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+
+
+class MainActivity : AppCompatActivity() {
+    lateinit var mGoogleSignInClient:GoogleSignInClient;
+    val RC_SIGN_IN=343;
+    val LOG_OUT=234;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Configure sign-in to request the user's ID, email address, and basic
+// profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        // Configure sign-in to request the user's ID, email address, and basic
+// profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        val sign_in_button:Button=findViewById(R.id.sign_in_button)
+
+        sign_in_button.setOnClickListener {
+            val signInIntent = mGoogleSignInClient.getSignInIntent();
+            startActivityForResult(signInIntent, RC_SIGN_IN);
+        }
+    }
+}
